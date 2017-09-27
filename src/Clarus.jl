1f91cb1c99e3973564ca34a2593b86f726e90613
Liam T. Henry <liam_henry@clarusft.com>
2017-09-27 16:10:59 +0100

    Refactoring Clarus.jl Library

--- src/Clarus.jl
+++ src/Clarus.jl
@@ -1,79 +1,9 @@
 module Clarus
 
 
-import Requests
-export api_request, api_key, api_secret
-
-type ApiConfig
-  key::String
-  secret::String
-end
-
-
-
-function keypath()
-  if Sys.is_windows()
-    return joinpath("c:/","clarusft","keys")
-  else
-    return joinpath(homedir(),"clarusft","keys")
-  end
-end
-
-
-const EMPTY           = ""
-credentials           = ApiConfig(EMPTY,EMPTY)
-const KEYFILE         = "API-Key.txt"
-const SECRETFILE      =  "API-Secret.txt"
-
-function _api_key!(c::ApiConfig)
-  if length(c.key) == 0
-    c.key = readCredentialFile(KEYFILE)
-  end
-  return c.key
-end
-
-function _api_secret!(c::ApiConfig)
-  if length(c.secret) == 0
-    c.secret = readCredentialFile(SECRETFILE)
-  end
-  return c.secret
-end
-
-function readCredentialFile(filename::String)
-  filepath = joinpath(keypath(),filename)
-  if isfile(filepath)
-    f = open(filepath)
-    try
-      x = strip(readstring(f))
-      return x
-    finally
-      close(f)
-    end
-  end
-  return EMPTY
-end
-
-
-function api_key!(key)
-  credentials.key = key
-end
-
-function api_secret!(secret)
-  credentials.secret = secret
-end
-
-
-function api_request(category, functionName; params...)
-  urlBase = "https://" * _api_key!(credentials) * ":" * _api_secret!(credentials) * "@apieval.clarusft.com/api/rest/v1/"
-  restUrl  =  urlBase * category * "/" * functionName * ".csv"
-  r = Requests.post(restUrl, json=Dict(params))
-  if Requests.statuscode(r)!=200
-    error("Request to " * category * "/" * functionName * " failed with status code: " * string(Requests.statuscode(r)))
-  end
-  return r
-end
 
 #SUB-MODULES
+include("Services.jl")
 include("Simm.jl")
 include("Margin.jl")
 include("Credit.jl")
@@ -91,5 +21,8 @@ include("Dates.jl")
 include("Util.jl")
 include("Trade.jl")
 #
+
+using Clarus.Services: api_key!, api_request, api_secret!
+#import Trade
 
 end #MODULE-END
--- src/Trade.jl
+++ src/Trade.jl
@@ -3,19 +3,19 @@ module Trade
 import ..Clarus
 
 function convert(;params...)
-  return Clarus.api_request("Trade","Convert";params...)
+  return Clarus.Services.api_request("Trade","Convert";params...)
 end
 
 function price(;params...)
-  return Clarus.api_request("Trade","Price";params...)
+  return Clarus.Services.api_request("Trade","Price";params...)
 end
 
 function cashflows(;params...)
-  return Clarus.api_request("Trade","Cashflows";params...)
+  return Clarus.Services.api_request("Trade","Cashflows";params...)
 end
 
 function validate(;params...)
-  return Clarus.api_request("Trade","Validate";params...)
+  return Clarus.Services.api_request("Trade","Validate";params...)
 end
 
 end #MODULE-END
