b261014baf48266f8eb0e9df8aa17ecefe5a44fa
Liam T. Henry <liam_henry@clarusft.com>
2017-10-02 16:04:14 +0100

    Added functions to retrieve stats from httpresponse

--- src/Response.jl
+++ src/Response.jl
@@ -1,17 +1,35 @@
-
+#MODULE: SERVICES
 import HttpCommon
 import DataFrames
 import CSV
 
 using DataFrames: DataFrame
 
-export Response, read, dataframe
+export Response, read, dataframe!, stats!, warnings
+
+#GLOBALS
+const STATS        = "X-Clarus-Stats"
+const WARNINGS     = "X-Clarus-Messages"
+const MESSAGES     = "messaged"
+
+#REQUEST STATS
+const GRID_TOTAL   = "GridTotal"
+const REQUEST_ID   = "RequestId"
+const GRID_ID      = "GridId"
+const CALC_TIME    = "CalcTime"
+const TASK_TIME    = "TaskTime"
+const NUM_MTM      = "NumMTM"
+const NUM_TRADES   = "NumTrades"
+const NUM_WARNINGS = "NumWarnings"
+
+#END-GLOBALS
+
 
 mutable struct Response
   httpresponse::HttpCommon.Response
-  stats
+  stats::Nullable{Dict{String,String}}
   dataframe::Nullable{DataFrame}
-  Response(r::HttpCommon.Response) = new(r,nothing,Nullable{DataFrames.DataFrame}())
+  Response(r::HttpCommon.Response) = new(r,Nullable{Dict{String,String}}(),Nullable{DataFrames.DataFrame}())
 end
 
 function text(r::Response)
@@ -47,4 +65,41 @@ end
 
 function Base.show(io::IO, r::Response)
   return Base.show(dataframe!(r))
+end
+
+function stats!(r::Response)
+  if isnull(r.stats)
+    head = r.httpresponse.headers
+    if STATS in keys(head)
+      d = Dict()
+      for (i,s) in enumerate(split(head[STATS],';'))
+          pair = split(s,'=')
+          push!(d, strip(pair[1])=> strip(pair[2]))
+      end
+      r.stats = Nullable(d)
+    end
+  end
+  return get(r.stats)
+end
+
+
+#getters
+function warnings(r::Response)
+  return r.httpresponse.headers[WARNINGS]
+end
+
+function stat(r::Response,stat::String)
+  return stats!(r)[stat]
+end
+
+function total(r::Response)
+  return stat(r, GRID_TOTAL)
+end
+
+function requestid(r::Response)
+  return stat(r,REQUEST_ID)
+end
+
+function gridid(r::Response)
+  return stat(r,GRID_ID)
 end
